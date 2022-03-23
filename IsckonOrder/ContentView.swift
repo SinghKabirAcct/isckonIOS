71fd6a7491a8199c150bb74f191bb5b9c42eee1e
Kabir Singh <sahai.kabir1@gmail.com>
2022-03-22 20:38:45 -0700

    Initial Commit

---
+++ IsckonOrder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IsckonOrder
+//
+//  Created by Kabir Singh on 3/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IsckonOrder/IsckonOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsckonOrderApp.swift
+//  IsckonOrder
+//
+//  Created by Kabir Singh on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct IsckonOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
